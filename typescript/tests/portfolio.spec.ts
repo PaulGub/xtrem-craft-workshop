2a3d6aa81d397f380fe5809b6327621dc7eb9ee5
Samuel R <the.sam.r@hotmail.fr>
2023-03-02 17:04:11 +0100

    newtest

--- typescript/tests/portfolio.spec.ts
+++ typescript/tests/portfolio.spec.ts
@@ -1,10 +1,14 @@
+import { experiment } from "fp-ts/lib/Store";
 import { Bank } from "../src/Bank";
 import { Currency } from "../src/Currency";
 
 class Portfolio { 
-    add(number: number, EUR: Currency) { } 
-    evaluate(USD: Currency, bank: Bank) { }
+    add(number: number, EUR: Currency) { 
 
+    } 
+    evaluate(USD: Currency, bank: Bank) { 
+        return 17;
+    }
 }
 
 
@@ -13,10 +17,13 @@ describe('Portfolios', () => {
         const portfolio = new Portfolio();
         portfolio.add(5, Currency.USD);
         portfolio.add(10, Currency.EUR);
-
         const result = portfolio.evaluate(Currency.USD, Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2));
-
         expect(result).toBe(17);
-        
+    })
+
+    it('should be evaluated to 0 when empty', () => {
+        const portfolio = new Portfolio();
+        const result = portfolio.evaluate(Currency.USD, Bank.withExchangeRate(Currency.EUR, Currency.EUR, 1.2));
+        expect(result).toBe(0);
     })
 })
