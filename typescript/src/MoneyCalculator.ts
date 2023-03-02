dc867e33df8d80d5fcd2140abddad0b8ffd45add
tjamard <jamard.theo17@gmail.com>
2023-03-02 14:29:11 +0100

    update convert method and MoneyCalculator.ts class

--- typescript/src/MoneyCalculator.ts
+++ typescript/src/MoneyCalculator.ts
@@ -1,7 +1,16 @@
 import { Currency } from './Currency'
 
 export class MoneyCalculator {
-  static Add = (amount: number, currency: Currency, amount2: number): number => amount + amount2
-  static Times = (amount: number, currency: Currency, number: number): number => amount * number
-  static Divide = (amount: number, currency: Currency, value: number): number => amount / value
+  private static checkNumberSign (num1: number, num2: number): boolean {
+    return Math.sign(num1) !== -1 && Math.sign(num2) !== -1
+  }
+  static Add (amount: number, currency: Currency, amount2: number): number|null {
+    return this.checkNumberSign(amount, amount2) ? amount + amount2 : null
+  }
+  static Times (amount: number, currency: Currency, number: number): number|null {
+    return this.checkNumberSign(amount, number) ? amount * number : null
+  }
+  static Divide (amount: number, currency: Currency, value: number): number {
+    return this.checkNumberSign(amount, value) ? amount / value : null
+  }
 }
