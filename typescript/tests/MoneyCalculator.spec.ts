e188cf43d4589d5976efe5edc2f1fb3dccfa934c
Samuel R <the.sam.r@hotmail.fr>
2023-03-02 15:25:08 +0100

    04_test_anatomy et 05_mutation_testing

--- typescript/tests/MoneyCalculator.spec.ts
+++ typescript/tests/MoneyCalculator.spec.ts
@@ -1,17 +1,61 @@
-import { Currency } from '../src/Currency'
-import { MoneyCalculator } from '../src/MoneyCalculator'
+import { Currency } from '../src/Currency';
+import { MoneyCalculator } from '../src/MoneyCalculator';
 
-describe('Money', function () {
-  test('add in usd returns number', () => {
-    expect(MoneyCalculator.Add(5, Currency.USD, 10)).toBeNumber()
-    expect(MoneyCalculator.Add(5, Currency.USD, 10)).not.toBeNull()
-  })
+describe('MoneyCalculator', () => {
 
-  test('multiply in eur returns positive number', () => {
-    expect(MoneyCalculator.Times(10, Currency.EUR, 2)).toBeGreaterThan(0)
-  })
+  describe('Addition', () => {
+    test('should return a number when adding two amounts in USD', () => {
+      const amount1 = 5;
+      const amount2 = 10;
+      const currency = Currency.USD;
 
-  test('divide in korean won returns number', () => {
-    expect(1000.5, ).toBe(MoneyCalculator.Divide(4002, Currency.KRW, 4))
-  })
-})
+      const result = MoneyCalculator.Add(amount1, currency, amount2);
+
+      expect(typeof result).toBe('number');
+    });
+
+    test('should not return null when adding two amounts in USD', () => {
+      const amount1 = 5;
+      const amount2 = 10;
+      const currency = Currency.USD;
+
+      const result = MoneyCalculator.Add(amount1, currency, amount2);
+
+      expect(result).not.toBeNull();
+    });
+  });
+
+  describe('Multiplication', () => {
+    test('should return a positive number when multiplying an amount in EUR by a factor', () => {
+      const amount = 10;
+      const factor = 2;
+      const currency = Currency.EUR;
+
+      const result = MoneyCalculator.Times(amount, currency, factor);
+
+      expect(result).toBeGreaterThan(0);
+    });
+  });
+
+  describe('Division', () => {
+    test('should return a number when dividing an amount in KRW by a divisor', () => {
+      const amount = 4002;
+      const divisor = 4;
+      const currency = Currency.KRW;
+
+      const result = MoneyCalculator.Divide(amount, currency, divisor);
+
+      expect(typeof result).toBe('number');
+    });
+
+    test('should return the correct quotient when dividing an amount in KRW by a divisor', () => {
+      const amount = 4002;
+      const divisor = 4;
+      const currency = Currency.KRW;
+
+      const result = MoneyCalculator.Divide(amount, currency, divisor);
+
+      expect(result).toBeCloseTo(1000.5, 4);
+    });
+  });
+});
