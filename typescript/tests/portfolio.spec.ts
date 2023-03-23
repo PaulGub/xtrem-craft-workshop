3e7ab208b000387d78a90e46e01c52fa35efabc2
lucasfaget <lucasfaget98@gmail.com>
2023-03-23 17:20:50 +0100

    add bank with all exchange rates to fix portfolio test fails

--- typescript/tests/portfolio.spec.ts
+++ typescript/tests/portfolio.spec.ts
@@ -20,10 +20,12 @@ class Portfolio {
 }
 
 describe('Portfolios', () => {
-  const bankEURtoUSD = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
-  const bankUSDtoKR = Bank.withExchangeRate(Currency.USD, Currency.KRW, 1100)
-  const bankUSDtoEUR = Bank.withExchangeRate(Currency.USD, Currency.EUR, 0.82)
-  const bankEURtoKR = Bank.withExchangeRate(Currency.EUR, Currency.KRW, 1344)
+  const bank = new Bank();
+  
+  bank.AddExchangeRate(Currency.EUR, Currency.USD, 1.2)
+  bank.AddExchangeRate(Currency.USD, Currency.KRW, 1100)
+  bank.AddExchangeRate(Currency.USD, Currency.EUR, 0.82)
+  bank.AddExchangeRate(Currency.EUR, Currency.KRW, 1344)
 
   test('5 USD + 10 EUR = 17 USD', () => {
     const portfolio = new Portfolio()
@@ -31,17 +33,17 @@ describe('Portfolios', () => {
     portfolio.add(new Money(5, Currency.USD));
     portfolio.add(new Money(10, Currency.EUR));
 
-    const result = portfolio.evaluate(Currency.USD, bankEURtoUSD)
+    const result = portfolio.evaluate(Currency.USD, bank)
 
     expect(result).toStrictEqual(new Money(17,Currency.USD))
   })
 
   test('1 USD + 1100 KRW = 2200 KRW', () => {
     const portfolio = new Portfolio()
-    portfolio.add(new Money(1, Currency.EUR))
+    portfolio.add(new Money(1, Currency.USD))
     portfolio.add(new Money(1100, Currency.KRW))
 
-    const result = portfolio.evaluate(Currency.KRW, bankUSDtoKR)
+    const result = portfolio.evaluate(Currency.KRW, bank)
 
     expect(result).toStrictEqual(new Money(2200, Currency.KRW))
   })
@@ -51,7 +53,7 @@ describe('Portfolios', () => {
     portfolio.add(new Money(5, Currency.USD))
     portfolio.add(new Money(10, Currency.EUR))
 
-    const result = portfolio.evaluate(Currency.EUR, bankUSDtoEUR)
+    const result = portfolio.evaluate(Currency.EUR, bank)
 
     expect(result).toStrictEqual(new Money(14.1, Currency.EUR))
   })
@@ -60,14 +62,17 @@ describe('Portfolios', () => {
     const portfolio = new Portfolio()
     portfolio.add(new Money(5, Currency.USD))
     portfolio.add(new Money(10, Currency.EUR))
-    const result2 = portfolio.evaluate(Currency.KRW, bankEURtoKR)
+    const bank = new Bank();
+    bank.AddExchangeRate(Currency.USD, Currency.KRW, 1100)
+    bank.AddExchangeRate(Currency.EUR, Currency.KRW, 1344)
+    const result2 = portfolio.evaluate(Currency.KRW, bank)
 
     expect(result2).toStrictEqual(new Money(18940, Currency.KRW))
   })
 
   it('should be evaluated to 0 when empty', () => {
     const portfolio = new Portfolio()
-    const result = portfolio.evaluate(Currency.USD, bankUSDtoKR)
+    const result = portfolio.evaluate(Currency.USD, bank)
 
     expect(result).toStrictEqual(new Money(0,Currency.USD))
   })
