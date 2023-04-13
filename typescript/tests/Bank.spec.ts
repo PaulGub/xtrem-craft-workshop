b6b7b30898e803bc786fe79501796eadde9f0966
lucasfaget <lucasfaget98@gmail.com>
2023-04-13 17:15:05 +0200

    create BankBuilder

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -2,7 +2,36 @@ import { Currency } from '../src/Currency';
 import { Bank } from '../src/Bank';
 import { MissingExchangeRateError } from '../src/MissingExchangeRateError';
 
+class BankBuilder {
+  static aBank = (): BankBuilder => new BankBuilder()
+  private pivotCurrency: Currency = Currency.EUR;
+  private exchangeRates: Map<Currency, number> = new Map<Currency, number>([
+    [Currency.USD, 1.2]
+  ]);
+
+  public withPivotCurrency(currency: Currency): BankBuilder {
+    this.pivotCurrency = currency;
+    return this;
+  }
+
+  public withExchangeRate(to: Currency, rate: number): BankBuilder {
+    this.exchangeRates.set(to, rate);
+    return this;
+  }
+
+  public build(): Bank {
+    let bank = new Bank;
+    this.exchangeRates.forEach((rate: number, currency: Currency) => {
+      bank.AddExchangeRate(this.pivotCurrency, currency, rate);
+      bank.AddExchangeRate(currency, this.pivotCurrency, 1/rate);
+    })
+    return bank;
+  }
+}
+
 describe('Bank', () => {
+
+  let builder: BankBuilder = BankBuilder.aBank().withPivotCurrency(Currency.EUR);
 
   describe('Conversion', () => {
     test('should convert an amount from EUR to USD using the provided exchange rate', () => {
@@ -11,7 +40,8 @@ describe('Bank', () => {
       const exchangeRate = 1.2;
       const amount = 10;
 
-      const result = Bank.withExchangeRate(fromCurrency, toCurrency, exchangeRate).Convert(amount, fromCurrency, toCurrency);
+      const bank = builder.withExchangeRate(Currency.USD, 1.2).build()
+      const result = bank.Convert(amount, fromCurrency, toCurrency);
 
       expect(result).toBe(12);
     });
@@ -22,13 +52,14 @@ describe('Bank', () => {
       const exchangeRate = 1;
       const amount = 10;
 
-      const result = Bank.withExchangeRate(fromCurrency, toCurrency, exchangeRate).Convert(amount, fromCurrency, toCurrency);
+      const bank = builder.withExchangeRate(Currency.USD, 1.2).build()
+      const result = bank.Convert(amount, fromCurrency, toCurrency);
 
       expect(result).toBe(amount);
     });
 
     test('convert throws error in case of missing exchange rates', () => {
-      expect(() => Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.KRW))
+      expect(() => builder.withExchangeRate(Currency.USD, 1.2).build().Convert(10, Currency.EUR, Currency.KRW))
         .toThrow(MissingExchangeRateError)
     })
 
@@ -37,9 +68,9 @@ describe('Bank', () => {
       const toCurrency = Currency.USD;
       const amount = 10;
 
-      const result1 = Bank.withExchangeRate(fromCurrency, toCurrency, 1.2).Convert(amount, fromCurrency, toCurrency);
-      const result2 = Bank.withExchangeRate(fromCurrency, toCurrency, 1.3).Convert(amount, fromCurrency, toCurrency);
-      const result3 = Bank.withExchangeRate(fromCurrency, toCurrency, 1.5).Convert(amount, fromCurrency, toCurrency);
+      const result1 = builder.withExchangeRate(Currency.USD, 1.2).build().Convert(amount, fromCurrency, toCurrency);
+      const result2 = builder.withExchangeRate(Currency.USD, 1.3).build().Convert(amount, fromCurrency, toCurrency);
+      const result3 = builder.withExchangeRate(Currency.USD, 1.5).build().Convert(amount, fromCurrency, toCurrency);
 
       expect(result1).toBe(12);
       expect(result2).toBe(13);
